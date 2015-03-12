3463d91bbc826168c4cae5364298d9f853950232
Jim Avery <jlavery@us.ibm.com>
2015-03-12 12:11:13 -0500

    Updated README, added license, removed JSLint warnings

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# MILTextExpander
+MILTextExpander
+===============
+
+<p align="center">
+<img src="mil-text-expander.gif" alt="Animation" border=1 /></p>
+
+MILTextExpander is a reusable UI component written as an AngularJS directive, which defines a __text expander__ HTML element. The text expander creates a section of text that can smoothly appear or disappear when the header is clicked. An additional icon can be specified for the right of the header as well, with optional rotation when the text is revealed.
+
+## Installation
+
+Include the .js file in the `head` of the HTML file where the AngularJS controller is being applied.
+
+```html
+<script src="[path to file]/text-expander.js"></script>
+```
+
+Also include the `MILTextExpander` module as a dependency when defining your AngularJS module.
+
+```js
+angular.module('myModule', ['MILTextExpander']);
+```
+
+## Usage
+
+The text expander can be inserted into an HTML document as in the example below:
+
+```html
+<text-expander title="Text Title" text="Lorem ipsum" button-image="arrow-button.jpg" button-rotation="90" />
+```
+
+* `title` - This parameter specifies the text to be displayed in the header.
+* `text` - This parameter specifies the text to be displayed in the body.
+* `button-image` - This optional parameter specifies an image to be displayed on the right side of the header.
+* `button-rotation` - This optional parameter specifies the number of degrees the `button-image` will rotate when the text is revealed. The default value is 0 degrees. Positive values indicate a clockwise rotation; negative values indicate a counterclockwise rotation.
+
+The text expander can be simply styled by making changes to the class `titleText` for the header, `detailsText` for the main text box, or `expandButton` for the optional header image. For more significant style adjustments, especially to the animation itself, modifying the directive template itself is recommended. Refer to the AngularJS documentation for [animations](https://docs.angularjs.org/guide/animations) and [ngAnimate](https://docs.angularjs.org/api/ngAnimate) for more information.
+
+## Requirements
+
+`MILTextExpander` is designed to work with AngularJS 1.3. It may work on older versions of AngularJS with little to no adjustment.
+
+`MILTextExpander` requires the `ngAnimate` module in order to animate the revealing of the text.
+
+## Author
+
+Created by [Jim Avery](https://github.com/TheSoundDefense) at the [IBM Mobile Innovation Lab](http://www-969.ibm.com/innovation/milab/)
+
+## License
+
+`MILTextExpander` is available under the Apache 2.0 license. See the LICENSE file for more info.
--- text-expander.js
+++ text-expander.js
@@ -15,43 +15,47 @@
  *  @copyright © 2015 IBM Corporation. All Rights Reserved.
  */
 
-'use strict';
+var angular;
 
-var MILTextExpander = angular.module('MILTextExpander', ['ngAnimate']);
+(function () {
+    'use strict';
 
-/**
- *  @class MILTextExpander.textExpander
- *  @memberOf MILTextExpander
- *  @description A directive that creates a text box that smoothly reveals and hides itself when the
- *  title area is clicked. This directive is decorated with four attributes: {@linkcode title} specifies the text
- *  to be displayed in the title area. {@linkcode text} specifies the text that will be shown or hidden.
- *  {@linkcode buttonImage} is an optional attribute for specifying an image that will exist to the right of the
- *  title. {@linkcode buttonRotation} is an optional attribute to specify the number of degrees the
- *  {@linkcode buttonImage} will rotate when the text is shown; negative values indicate counterclockwise rotation.
- *  @example
- *  <text-expander title="Lorem" text="Ipsum" button-image="arrow-button.jpg" button-rotation="90"></text-expander>
- *  @author James Avery
- *  @copyright © 2015 IBM Corporation. All Rights Reserved.
- */
-MILTextExpander.directive('textExpander', function () {
+    var MILTextExpander = angular.module('MILTextExpander', ['ngAnimate']);
 
-    return {
+    /**
+     *  @class MILTextExpander.textExpander
+     *  @memberOf MILTextExpander
+     *  @description A directive that creates a text box that smoothly reveals and hides itself when the
+     *  title area is clicked. This directive is decorated with four attributes: {@linkcode title} specifies the text
+     *  to be displayed in the title area. {@linkcode text} specifies the text that will be shown or hidden.
+     *  {@linkcode buttonImage} is an optional attribute for specifying an image that will exist to the right of the
+     *  title. {@linkcode buttonRotation} is an optional attribute to specify the number of degrees the
+     *  {@linkcode buttonImage} will rotate when the text is shown; negative values indicate counterclockwise rotation.
+     *  @example
+     *  <text-expander title="Lorem" text="Ipsum" button-image="arrow-button.jpg" button-rotation="90"></text-expander>
+     *  @author James Avery
+     *  @copyright © 2015 IBM Corporation. All Rights Reserved.
+     */
+    MILTextExpander.directive('textExpander', function () {
 
-        restrict: 'E',
+        return {
 
-        scope: {
-            title: '@',
-            text: '@',
-            buttonImage: '@',
-            buttonRotation: '@'
-        },
+            restrict: 'E',
 
-        template: "<style>.titleText{font-size:18pt;margin-bottom: 25px;}.titleText span:last-child{float: right;}.expandButton{height: auto; width: 18px;-webkit-transition: all ease 0.3s;transition: all ease 0.3s;}.detailsText{margin-bottom: 25px;font-family: 'OpenSansRegular';font-size: 14pt;}.rotateButton{-webkit-transform: rotate({{buttonRotation}}deg);transform: rotate({{buttonRotation}}deg);}.hideAnimation.ng-hide-add, .hideAnimation.ng-hide-remove{-webkit-transition: all ease 0.6s;transition: all ease 0.6s;}.hideAnimation.ng-hide-add{max-height: 300px;margin-bottom: 25px;overflow: hidden;}.hideAnimation.ng-hide-add-active{max-height: 0px;margin-bottom: 0px;}.hideAnimation.ng-hide-remove{max-height: 0px;margin-bottom: 0px;overflow: hidden;}.hideAnimation.ng-hide-remove-active{max-height: 300px;margin-bottom: 25px;}</style><div class='titleText' ng-click='displayText = !displayText'><span>{{title}}</span><span><img class='expandButton' src='{{buttonImage}}' ng-class='{rotateButton: displayText}'></span></div><div class='detailsText hideAnimation' ng-show='displayText'>{{text}}</div>",
+            scope: {
+                title: '@',
+                text: '@',
+                buttonImage: '@',
+                buttonRotation: '@'
+            },
 
-        link: function (scope, element, attrs) {
-            scope.displayText = false;
-        }
+            template: "<style>.titleText{margin-bottom: 25px;}.titleText span:last-child{float: right;}.expandButton{height: auto;-webkit-transition: all ease 0.3s;transition: all ease 0.3s;}.detailsText{}.rotateButton{-webkit-transform: rotate({{buttonRotation}}deg);transform: rotate({{buttonRotation}}deg);}.hideAnimation.ng-hide-add, .hideAnimation.ng-hide-remove{-webkit-transition: all ease 0.6s;transition: all ease 0.6s;}.hideAnimation.ng-hide-add{max-height: 300px;overflow: hidden;}.hideAnimation.ng-hide-add-active{max-height: 0px;}.hideAnimation.ng-hide-remove{max-height: 0px;overflow: hidden;}.hideAnimation.ng-hide-remove-active{max-height: 300px;}</style><div class='titleText' ng-click='displayText = !displayText'><span>{{title}}</span><span><img class='expandButton' src='{{buttonImage}}' ng-class='{rotateButton: displayText}'></span></div><div class='detailsText hideAnimation' ng-show='displayText'>{{text}}</div>",
 
-    };
+            link: function (scope, element, attrs) {
+                scope.displayText = false;
+            }
 
-});
+        };
+
+    });
+}());
